61007779791c2ffb2b97c8005c2d2782de073f32
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-05-11 09:08:59 +0530

    feat: add models for video rental

--- videos/models.py
+++ videos/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=256)
+    length = models.PositiveIntegerField()
+    release_year = models.PositiveIntegerField()
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    phone = models.PositiveIntegerField()
